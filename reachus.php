d724fdda0d54c005fee0bfc80ecbac301ef3aba8
landuyt <maarten.landuyt@skryv.com>
2015-04-28 19:44:25 +0200

    update of form and css cleanup

--- reachus.php
+++ reachus.php
@@ -6,19 +6,20 @@
     $msg_subject    = $_POST['subject'];
     $msgs     = $_POST['msg'];
     
-    if($name !='' && $email!='' && $phone !='' && $msg_subject !='' && $msgs !=''){
+    if($name !='' && $email!='' && $msg_subject !='' && $msgs !=''){
         $to = "agapatsa@gmail.com";
-	$subject = "Contact Mail";
-	$message = "<p>Dear Admin,</p><p>You have a contact request </p><p>Please find the below information</p>
-                    <p>Name :".$name."</p>
-                    <p>Email :".$email."</p>
-                    <p>Phone :".$phone."</p>
-                    <p>subject :".$msg_subject."</p>
-                    <p>Message:</p><p>".$msgs."</p>";
-	$header .= 'To: '.$name.' '.$email. "\r\n";
-	$header  = "From:".$email."\r\n";
-	$header .= "MIME-Version: 1.0\r\n";
-	$header .= "Content-type: text/html\r\n";
+		$subject = "Website - Wedding Mail from ".$name." about ".$msg_subject;
+		$message = "<p>Dear Agapi,</p>
+					<p>You have a contact request </p><p>Please find the below information</p>
+	                    <p>Name :".$name."</p>
+	                    <p>Email :".$email."</p>
+	                    <p>Phone :".$phone."</p>
+	                    <p>subject :".$msg_subject."</p>
+	                    <p>Message:</p><p>".$msgs."</p>";
+		$header .= 'To: '.$name.' '.$email. "\r\n";
+		$header  = "From:".$email."\r\n";
+		$header .= "MIME-Version: 1.0\r\n";
+		$header .= "Content-type: text/html\r\n";
 	$send = mail ($to,$subject,$message,$header);
         if($send){
 	$msg = "ok"; 
